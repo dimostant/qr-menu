eee08cf139674d8f0e1561b56dfe1d947f0c3d92
dimostant <dimos.2000@hotmail.com>
2026-04-21 14:53:38 +0300

    add pages placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
-import SearchBar from './components/search_bar'
+import "./App.css";
+import Navbar from "./components/navbar";
+import Page from "./components/search_bar";
 
 function App() {
   return (
     <>
-      <SearchBar></SearchBar>
+      <Navbar></Navbar>
+      <div className="w-screen justify-center">
+        {[...Array(10)].map(() => (
+          <div>
+            <Page></Page>
+            <div className="w-2 h-2" />
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,5 @@
+function NavBar() {
+  return <div className="h-12"></div>;
+}
+
+export default NavBar;
