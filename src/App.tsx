3070addabcf761d21865286a6f3e7e63e792c72b
dimostant <dimos.2000@hotmail.com>
2026-04-25 23:09:58 +0300

    Add navbar with name and search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import Page from "./components/search_bar";
+import { useRef } from "react";
 
 function App() {
+  const pages = useRef(10);
+
   return (
     <>
       <Navbar></Navbar>
+      <div className="h-2"/>
       <div className="w-screen justify-center">
-        {[...Array(10)].map(() => (
+        {[...Array(pages.current)].map(() => (
           <div>
             <Page></Page>
             <div className="w-2 h-2" />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,13 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 function NavBar() {
-  return <div className="h-12"></div>;
+  return (
+    <div className="w-screen h-12 bg-gray-300 grid grid-cols-2 justify-center place-items-center">
+      <div className="text-lg">QR Menu</div>
+      <FaMagnifyingGlass/>
+    </div>
+    
+  )
 }
 
 export default NavBar;
