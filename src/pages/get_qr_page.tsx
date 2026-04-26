97fcc06f1defa76d6a42fe392fd4292e3540add3
dimostant <dimos.2000@hotmail.com>
2026-04-26 20:35:10 +0300

    setup shadcn, use attach image component

--- src/pages/get_qr_page.tsx
+++ src/pages/get_qr_page.tsx
@@ -1,5 +1,11 @@
+import Example from '@/components/file-upload-dropzone-4'
+
 function GetQrPage() {
-  return <>GetQrPage</>
+  return (
+    <div className="w-screen h-screen place-items-center justify-items-center">
+      <Example />
+    </div>
+  )
 }
 
 export default GetQrPage
